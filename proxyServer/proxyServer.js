86b268659d303ceda1fcd65b8051687b75d762f9
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-06-13 21:06:12 -0700

    Rendered all four components

--- proxyServer/proxyServer.js
+++ proxyServer/proxyServer.js
@@ -7,19 +7,61 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, './')));
 
-//Descriptions Server
-app.use('/rooms/:pathname', function(req, res) {
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header(
+      'Access-Control-Allow-Mehods',
+      'GET, POST, PUT, PATCH, DELETE',
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
+//Descriptions Server bundle and CSS
+app.use('/bookingFiles/:pathname', function(req, res) {
+  res.redirect(307, 'http://localhost:3002/' + req.params.pathname)
+});
+
+//Descriptions Services
+app.use('/booking/', function(req, res) {
+  res.redirect(307, 'http://localhost:3002/booking' + req.url)
+});
+
+//listingDescriptionFiles Server bundle and CSS
+app.use('/listingDescriptionFiles/:pathname', function(req, res) {
   res.redirect(307, 'http://localhost:3001/' + req.params.pathname)
 });
 
-//Reviews Server
-app.use('/reviews/:pathname', function(req, res) {
+//listingDescriptionFiles Services
+app.use('/rooms/', function(req, res) {
+  res.redirect(307, 'http://localhost:3001' + req.url)
+});
+
+//Reviews Server bundle and CSS
+app.use('/reviewsFiles/:pathname', function(req, res) {
   res.redirect(307, 'http://localhost:3003/' + req.params.pathname)
 });
 
-//Filter Listings Server
-app.use('/filterListings/:pathname', function(req, res) {
+//Reviews Services
+app.use('/reviews/', function(req, res) {
+  console.log(req.params.pathname)
+  res.redirect(307, 'http://localhost:3003/reviews' + req.url)
+});
+
+//Filter Listings Server bundle and CSS
+app.use('/filterListingsFiles/:pathname', function(req, res) {
   res.redirect(307, 'http://localhost:3004/' + req.params.pathname)
+});
+
+//Filter Listings Services
+app.use('/filterListings/', function(req, res) {
+  res.redirect(307, 'http://localhost:3004' + req.url)
 });
 
 app.listen(3000, () => {
